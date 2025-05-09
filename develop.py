41f59b9a1d475a7906cf53c497a7ead2c8737725
SHB <shb1998@naver.com>
2025-05-09 11:11:23 +0900

    test from test branch

--- develop.py
+++ develop.py
@@ -1,2 +1,8 @@
 print('Hello from develop.py')
 print('This is a test for the develop branch')
+
+print('This is a test for the test branch')
+print('This is a test for the test branch')
+print('This is a test for the test branch')
+print('This is a test for the test branch')
+print('This is a test for the test branch')
